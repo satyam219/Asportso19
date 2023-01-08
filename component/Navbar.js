6b0ae2adabe0512d07413a389b67fe83ed7b8abe
satyam219 <asportsomania@gmail.com>
2023-01-08 13:01:04 +0530

    080123 Commit

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import Link from 'next/link'
 //import styles from '../styles/Home.module.css'
 import styles from '../styles/Nav.module.css'
+import Link  from 'next/link'
+ const Navbar = () =>{
+return (
+  <>
 
-const Navbar = () => {
-  return ( 
-    <>
-        <nav className={styles.mainNav1}>
-          
-    <ul className={styles.lin}>
-       <Link href='/'><li>Home</li></Link>
-       <Link href='/blog'><li>Blog</li></Link>
-       <Link href='/about'><li>About</li></Link>
-       <Link href='/contact'><li>contact</li></Link>
-    </ul>
-  </nav>
-  <h1 className={styles.title}>
-          <span className='mySpan'>Asportsomania</span>
-        </h1>
-        <img className ={styles.image} src="soccer.png" alt="" />
-        <img className={styles.image1} src="humburg.png" alt="" />
+    <nav className={styles.mainNav1}>
+
+      <ul className={styles.lin}>
+        <Link href='/'><li>Home</li></Link>
+        <Link href='/blog'><li>Blog</li></Link>
+        <Link href='/about'><li>About</li></Link>
+        <Link href='/contact'><li>contact</li></Link>
+      </ul>
+    </nav>
+    <h1 className={styles.title}>
+      <span className='mySpan'>Asportsomania</span>
+    </h1>
+    <img className={styles.image} src="soccer.png" alt="" />
   </>
-  )
-}
+)
+ }
 
 export default Navbar
